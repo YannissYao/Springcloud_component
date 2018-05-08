6907571e7ce5fffde2345a993a7f3301fad35ce9
Joeysin <834395851@qq.com>
2018-05-08 18:34:52 +0800

    repair some bug

--- microservice-order-service-with-sleuthZipkin-backup/src/main/java/com/springCloud/OrderServiceBackUpApplication.java
+++ microservice-order-service-with-sleuthZipkin-backup/src/main/java/com/springCloud/OrderServiceBackUpApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class OrderServiceBackUpApplication {
 
 
-
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceBackUpApplication.class, args);
     }
--- microservice-sleuth-zipkin-service/src/main/java/com/springCloud/SleuthzipkinApplication.java
+++ microservice-sleuth-zipkin-service/src/main/java/com/springCloud/SleuthzipkinApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+
 
 /**
  * Created by Joeysin on  2018/2/26  下午4:12.
@@ -15,11 +14,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableZipkinStreamServer
 @EnableEurekaClient
 public class SleuthzipkinApplication {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SleuthzipkinApplication.class, args);
